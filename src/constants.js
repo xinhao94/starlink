aec57c721ed8181d72486f7a47fa4a3de8e30b92
Xinhao Song <songxinhao@hotmail.com>
2021-03-28 18:29:32 -0500

    Add URL for drawing the world map

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const SAT_API_KEY = "NGBQZ2-95EQ5Q-ZTE3F5-4NOW";
 export const STARLINK_CATEGORY = "52";
 
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
